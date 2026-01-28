826d595ad25b493424782d941b483a36c9660b8f
jonascarris <jonascarris@gmail.com>
2026-01-28 14:04:27 -0300

    Add files via upload

--- server/config/mercadopago.js
+++ server/config/mercadopago.js
@@ -2,7 +2,7 @@ import { MercadoPagoConfig } from 'mercadopago';
 
 // Configuração do Mercado Pago com as credenciais fornecidas
 const client = new MercadoPagoConfig({
-    accessToken: 'APP_USR-8109231522244005-052614-8787b612fa04e743e0dda33b1099b7fc-10939427',
+    accessToken: process.env.MP_ACCESS_TOKEN || 'APP_USR-8109231522244005-052614-8787b612fa04e743e0dda33b1099b7fc-10939427',
     options: { timeout: 5000 }
 });
 
